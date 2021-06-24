8d763de5472b4c7e1417548077e96aa530b768b5
Enrico Heber <eheber@compeso.com>
2021-06-24 11:49:45 +0200

    RomanToIntegerHelper

---
+++ LeetCodeChallenge/RomanToIntegerHelper.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeChallenge
+{
+    public class RomanToIntegerHelper
+    {
+        public int RomanToInt(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            int charArrayLength = charArray.Length;
+
+            int ret = 0;
+
+            for (int index = 0; index < charArrayLength; ++index)
+            {
+                char c = charArray[index];
+                char nextChar = index + 1 < charArrayLength ? charArray[index + 1] : '\0';
+
+                //I can be placed before V(5) and X(10) to make 4 and 9.
+                //X can be placed before L(50) and C(100) to make 40 and 90.
+                //C can be placed before D(500) and M(1000) to make 400 and 900.
+                switch (c)
+                {
+                    case 'M':
+                        ret += 1000;
+                        break;
+
+                    case 'D':
+                        ret += 500;
+                        break;
+
+                    case 'C':
+                        if (nextChar == 'D' || nextChar == 'M')
+                        {
+                            ret -= 100;
+                        }
+                        else
+                        {
+                            ret += 100;
+                        }
+                        break;
+
+                    case 'L':
+                        ret += 50;
+                        break;
+
+                    case 'X':
+                        if (nextChar == 'L' || nextChar == 'C')
+                        {
+                            ret -= 10;
+                        }
+                        else
+                        {
+                            ret += 10;
+                        }
+                        break;
+
+                    case 'V':
+                        ret += 5;
+                        break;
+
+                    case 'I':
+                        if (nextChar == 'V' || nextChar == 'X')
+                        {
+                            ret -= 1;
+                        }
+                        else
+                        {
+                            ret += 1;
+                        }
+                        break;
+                }
+            }
+
+            return ret;
+        }
+    }
+}
---
+++ LeetCodeChallengeTest/RomanToIntegerHelperTest.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using LeetCodeChallenge;
+
+namespace LeetCodeChallengeTest
+{
+    class RomanToIntegerHelperTest
+    {
+        RomanToIntegerHelper romanToIntegerHelper;
+
+        [SetUp]
+        public void Setup()
+        {
+            romanToIntegerHelper = new RomanToIntegerHelper();
+        }
+
+        [Test]
+        public void TestAAA()
+        {
+            Assert.AreEqual(0, romanToIntegerHelper.RomanToInt(""));
+        }
+
+        [Test]
+        public void TestOrigEmpty()
+        {
+            Assert.AreEqual(0, romanToIntegerHelper.RomanToInt(""));
+        }
+
+        [Test]
+        public void TestOrig1()
+        {
+            Assert.AreEqual(3, romanToIntegerHelper.RomanToInt("III"));
+        }
+
+        [Test]
+        public void TestOrig2()
+        {
+            Assert.AreEqual(4, romanToIntegerHelper.RomanToInt("IV"));
+        }
+
+        [Test]
+        public void TestOrig3()
+        {
+            Assert.AreEqual(9, romanToIntegerHelper.RomanToInt("IX"));
+        }
+
+        [Test]
+        public void TestOrig4()
+        {
+            Assert.AreEqual(58, romanToIntegerHelper.RomanToInt("LVIII"));
+        }
+
+        [Test]
+        public void TestOrig5()
+        {
+            Assert.AreEqual(1994, romanToIntegerHelper.RomanToInt("MCMXCIV"));
+        }
+
+    }
+}
