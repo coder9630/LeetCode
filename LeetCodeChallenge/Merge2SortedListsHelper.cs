aaecb088b3e65f9b595ae7773949442acc215298
Enrico Heber <eheber@compeso.com>
2021-06-27 06:27:06 +0200

    -Merge2SortedListsHelper

---
+++ LeetCodeChallenge/Merge2SortedListsHelper.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeChallenge
+{
+
+    public class Merge2SortedListsHelper
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public class List
+        {
+            private ListNode headNode;
+
+            public List()
+            {
+            }
+
+            public ListNode HeadNode
+            {
+                get
+                {
+                    return this.headNode;
+                }
+                set
+                {
+                    this.headNode = value;
+                }
+            }
+
+            public static List CreateList(int[] values)
+            {
+                var list = new List();
+
+                if (values.Length == 0)
+                    return list;
+
+                list.headNode = new ListNode();
+                list.headNode.val = values[0];
+
+                var newEndNode = list.headNode;
+                for (int index = 1; index < values.Length; ++index)
+                {
+                    newEndNode.next = new ListNode();
+                    newEndNode = newEndNode.next;
+                    newEndNode.val = values[index];
+                }
+
+                return list;
+            }
+
+            public System.Collections.Generic.IEnumerable<ListNode> Nodes
+            {
+                get
+                {
+                    if (this.headNode != null)
+                    {
+                        yield return this.headNode;
+
+                        var endNode = this.headNode;
+                        while (endNode.next != null)
+                        {
+                            endNode = endNode.next;
+                            yield return endNode;
+                        }
+                    }
+                }
+            }
+
+            public int[] ToArray()
+            {
+                int count = this.Count();
+
+                if (count == 0)
+                    return new int[0];
+
+                int[] ret = new int[count];
+
+                int index = 0;
+                foreach (ListNode node in this.Nodes)
+                {
+                    ret[index] = node.val;
+                    ++index;
+                }
+
+                return ret;
+            }
+
+            public int Count()
+            {
+                int count = 0;
+                foreach (ListNode node in this.Nodes)
+                {
+                    ++count;
+                }
+                return count;
+            }
+
+            public ListNode FindNode(int searchIndex)
+            {
+                int index = 1;
+                foreach (ListNode node in this.Nodes)
+                {
+                    if (index == searchIndex)
+                        return node;
+                    ++index;
+                }
+
+                return null;
+            }
+        }
+
+        public ListNode MergeTwoLists(ListNode headNode1, ListNode headNode2)
+        {
+            if (headNode1 == null && headNode2 == null)
+                return null;
+
+            List retlist = new List();
+
+            ListNode currNode1 = headNode1;
+            ListNode currNode2 = headNode2;
+            ListNode retHeadNode = null;
+            ListNode newEndNode = null;
+
+            while (currNode1 != null || currNode2 != null)
+            {
+                int currVal = 0;
+                if (currNode1 != null && currNode2 == null)
+                {
+                    currVal = currNode1.val;
+                    currNode1 = currNode1.next;
+                }
+                else if (currNode1 == null && currNode2 != null)
+                {
+                    currVal = currNode2.val;
+                    currNode2 = currNode2.next;
+                }
+                else
+                {
+                    if (currNode1.val < currNode2.val)
+                    {
+                        currVal = currNode1.val;
+                        currNode1 = currNode1.next;
+                    }
+                    else if (currNode1.val > currNode2.val)
+                    {
+                        currVal = currNode2.val;
+                        currNode2 = currNode2.next;
+                    }
+                    else if (currNode1.val == currNode2.val)
+                    {
+                        currVal = currNode1.val;
+                        currNode1 = currNode1.next;
+                    }
+                }
+
+
+                if (retHeadNode == null)
+                {
+                    retHeadNode = new ListNode();
+                    retHeadNode.val = currVal;
+                    newEndNode = retHeadNode;
+                }
+                else
+                {
+                    newEndNode.next = new ListNode();
+                    newEndNode = newEndNode.next;
+                    newEndNode.val = currVal;
+                }
+            }
+
+            return retHeadNode;
+        }
+    }
+}
---
+++ LeetCodeChallengeTest/Merge2SortedListsHelperTest.cs
@@ -0,0 +1,121 @@
+﻿using NUnit.Framework;
+using LeetCodeChallenge;
+using System.Collections.Generic;
+using System.Drawing;
+using System;
+
+namespace LeetCodeChallengeTest
+{
+    class Merge2SortedListsHelperTest
+    {
+        Merge2SortedListsHelper merge2SortedListsHelper;
+
+        [SetUp]
+        public void Setup()
+        {
+            merge2SortedListsHelper = new Merge2SortedListsHelper();
+        }
+
+        [Test]
+        public void TestAAA()
+        {
+            var list1 = Merge2SortedListsHelper.List.CreateList(new int[] { });
+            var list2 = Merge2SortedListsHelper.List.CreateList(new int[] { });
+
+            var newList = new Merge2SortedListsHelper.List();
+            newList.HeadNode = merge2SortedListsHelper.MergeTwoLists(list1.HeadNode, list2.HeadNode);
+
+            Assert.AreEqual(null, newList.HeadNode);
+        }
+
+        [Test]
+        public void TestOrigEmpty()
+        {
+            var list1 = Merge2SortedListsHelper.List.CreateList(new int[] { });
+            var list2 = Merge2SortedListsHelper.List.CreateList(new int[] { });
+
+            var newList = new Merge2SortedListsHelper.List();
+            newList.HeadNode = merge2SortedListsHelper.MergeTwoLists(list1.HeadNode, list2.HeadNode);
+
+            Assert.AreEqual(null, newList.HeadNode);
+        }
+
+
+        [Test]
+        public void TestOrig1()
+        {
+            var list1 = Merge2SortedListsHelper.List.CreateList(new int[] { 1, 2, 4 });
+            var list2 = Merge2SortedListsHelper.List.CreateList(new int[] { 1, 3, 4 });
+
+            var newList = new Merge2SortedListsHelper.List();
+            newList.HeadNode = merge2SortedListsHelper.MergeTwoLists(list1.HeadNode, list2.HeadNode);
+
+            var arr = newList.ToArray();
+            Assert.AreEqual(6, arr.Length);
+            Assert.AreEqual(1, arr[0]);
+            Assert.AreEqual(1, arr[1]);
+            Assert.AreEqual(2, arr[2]);
+            Assert.AreEqual(3, arr[3]);
+            Assert.AreEqual(4, arr[4]);
+            Assert.AreEqual(4, arr[5]);
+        }
+
+        [Test]
+        public void TestEH1()
+        {
+            var list1 = Merge2SortedListsHelper.List.CreateList(new int[] { });
+            var list2 = Merge2SortedListsHelper.List.CreateList(new int[] { 1, 3, 4 });
+
+            var newList = new Merge2SortedListsHelper.List();
+            newList.HeadNode = merge2SortedListsHelper.MergeTwoLists(list1.HeadNode, list2.HeadNode);
+
+            var arr = newList.ToArray();
+            Assert.AreEqual(3, arr.Length);
+            Assert.AreEqual(1, arr[0]);
+            Assert.AreEqual(3, arr[1]);
+            Assert.AreEqual(4, arr[2]);
+        }
+
+        [Test]
+        public void TestEH2()
+        {
+            var list1 = Merge2SortedListsHelper.List.CreateList(new int[] { 1, 3, 4 });
+            var list2 = Merge2SortedListsHelper.List.CreateList(new int[] { });
+
+            var newList = new Merge2SortedListsHelper.List();
+            newList.HeadNode = merge2SortedListsHelper.MergeTwoLists(list1.HeadNode, list2.HeadNode);
+
+            var arr = newList.ToArray();
+            Assert.AreEqual(3, arr.Length);
+            Assert.AreEqual(1, arr[0]);
+            Assert.AreEqual(3, arr[1]);
+            Assert.AreEqual(4, arr[2]);
+        }
+
+        [Test]
+        public void TestEH3()
+        {
+            var list1 = Merge2SortedListsHelper.List.CreateList(new int[] { 1, 2, 3, 3, 3, 4 });
+            var list2 = Merge2SortedListsHelper.List.CreateList(new int[] { 1, 2, 2, 2, 3, 4 });
+
+            var newList = new Merge2SortedListsHelper.List();
+            newList.HeadNode = merge2SortedListsHelper.MergeTwoLists(list1.HeadNode, list2.HeadNode);
+
+            var arr = newList.ToArray();
+            Assert.AreEqual(12, arr.Length);
+            Assert.AreEqual(1, arr[0]);
+            Assert.AreEqual(1, arr[1]);
+            Assert.AreEqual(2, arr[2]);
+            Assert.AreEqual(2, arr[3]);
+            Assert.AreEqual(2, arr[4]);
+            Assert.AreEqual(2, arr[5]);
+            Assert.AreEqual(3, arr[6]);
+            Assert.AreEqual(3, arr[7]);
+            Assert.AreEqual(3, arr[8]);
+            Assert.AreEqual(3, arr[9]);
+            Assert.AreEqual(4, arr[10]);
+            Assert.AreEqual(4, arr[11]);
+        }
+
+    }
+}
